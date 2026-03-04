9a28dd1f9be10b5632a06412742245c95ee085bd
Letícia Stefanie Maciel Silva <stefanie04@uniplaclages.edu.br>
2026-03-04 20:45:02 -0300

    alteração

--- src/main.py
+++ src/main.py
@@ -10,3 +10,8 @@ app.include_router(FuncionarioRouter.router)
 app.include_router(ClienteRouter.router)
 if __name__ == "__main__":
     uvicorn.run('main:app', host=HOST, port=int(PORT), reload=RELOAD)
+
+#rota padrão
+@app.get("/", tags=["Root"], status_code=200)
+def root():
+    return {"detail":"API Pastelaria", "Swagger UI": "http://127.0.0.1:8000/docs", "ReDoc": "http://127.0.0.1:8000/redoc" }
